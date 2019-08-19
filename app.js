62eaaea071ff32fcee6812338b1afe66fb044511
sofiyamamchych <sofiyamamchich@gmail.com>
2019-08-19 14:13:42 +0300

    hero carousel draft - slick added

---
+++ app.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+    $('.hero__carousel__main').slick({
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        // autoplay: true,
+        autoplaySpeed: 3000,
+    });
+  });
